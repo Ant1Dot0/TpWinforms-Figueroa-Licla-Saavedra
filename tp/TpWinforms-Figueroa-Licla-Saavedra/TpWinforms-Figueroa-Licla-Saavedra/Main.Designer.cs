f5bdbefa44de5c31e1a545d94bc170a22d6f5328
GianfrancoFigueroa <figueroagian02@gmail.com>
2023-04-28 17:04:08 -0300

    Agregue las clases de Marcas, Categorias y Articulos, verifiquen si esta bien y si estan de acuerdo

---
+++ tp/TpWinforms-Figueroa-Licla-Saavedra/TpWinforms-Figueroa-Licla-Saavedra/Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TpWinforms_Figueroa_Licla_Saavedra
+{
+    partial class OrganizART
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListadoArtStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.altasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CargaArticulosStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.consultasToolStripMenuItem,
+            this.altasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(916, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // consultasToolStripMenuItem
+            // 
+            this.consultasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ListadoArtStripMenu});
+            this.consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            this.consultasToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // ListadoArtStripMenu
+            // 
+            this.ListadoArtStripMenu.Name = "ListadoArtStripMenu";
+            this.ListadoArtStripMenu.Size = new System.Drawing.Size(180, 22);
+            this.ListadoArtStripMenu.Text = "Listado de Articulos";
+            this.ListadoArtStripMenu.Click += new System.EventHandler(this.ListadoArtStripMenu_Click);
+            // 
+            // altasToolStripMenuItem
+            // 
+            this.altasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CargaArticulosStripMenu});
+            this.altasToolStripMenuItem.Name = "altasToolStripMenuItem";
+            this.altasToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.altasToolStripMenuItem.Text = "Altas";
+            // 
+            // CargaArticulosStripMenu
+            // 
+            this.CargaArticulosStripMenu.Name = "CargaArticulosStripMenu";
+            this.CargaArticulosStripMenu.Size = new System.Drawing.Size(180, 22);
+            this.CargaArticulosStripMenu.Text = "Carga de Articulos";
+            this.CargaArticulosStripMenu.Click += new System.EventHandler(this.CargaArticulosStripMenu_Click);
+            // 
+            // OrganizART
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 503);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "OrganizART";
+            this.Text = "OrganizART";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ListadoArtStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem CargaArticulosStripMenu;
+    }
+}
+
---
+++ tp/TpWinforms-Figueroa-Licla-Saavedra/TpWinforms-Figueroa-Licla-Saavedra/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TpWinforms_Figueroa_Licla_Saavedra
+{
+    public partial class OrganizART : Form
+    {
+        public OrganizART()
+        {
+            InitializeComponent();
+        }
+
+        private void ListadoArtStripMenu_Click(object sender, EventArgs e)
+        {
+            ListadoDeArticulos FormList = new ListadoDeArticulos();
+            FormList.ShowDialog();
+        }
+
+        private void CargaArticulosStripMenu_Click(object sender, EventArgs e)
+        {
+            CargaDeArticulos FormCarga = new CargaDeArticulos();
+            FormCarga.ShowDialog();
+        }
+    }
+}
