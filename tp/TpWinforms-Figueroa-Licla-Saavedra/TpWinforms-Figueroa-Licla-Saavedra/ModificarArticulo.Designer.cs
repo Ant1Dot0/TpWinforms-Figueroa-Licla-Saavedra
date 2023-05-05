7e6c14c08a6214a96aef69e9e0bc2cff4c99df83
Ant1Dot0 <joselicla021@gmail.com>
2023-05-05 00:58:32 -0300

    Form Modificar

---
+++ tp/TpWinforms-Figueroa-Licla-Saavedra/TpWinforms-Figueroa-Licla-Saavedra/ModificarArticulo.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace TpWinforms_Figueroa_Licla_Saavedra
+{
+    partial class ModificarArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtPrecio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnCargar = new System.Windows.Forms.Button();
+            this.cmxCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtCodArticulo = new System.Windows.Forms.TextBox();
+            this.lblCodigoArticulo = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtPrecio
+            // 
+            this.TxtPrecio.Location = new System.Drawing.Point(171, 252);
+            this.TxtPrecio.Name = "TxtPrecio";
+            this.TxtPrecio.Size = new System.Drawing.Size(121, 20);
+            this.TxtPrecio.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Precio:";
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancel.Location = new System.Drawing.Point(454, 332);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(118, 39);
+            this.BtnCancel.TabIndex = 25;
+            this.BtnCancel.Text = "CANCELAR";
+            this.BtnCancel.UseVisualStyleBackColor = false;
+            // 
+            // BtnCargar
+            // 
+            this.BtnCargar.BackColor = System.Drawing.Color.YellowGreen;
+            this.BtnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCargar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BtnCargar.Location = new System.Drawing.Point(48, 332);
+            this.BtnCargar.Name = "BtnCargar";
+            this.BtnCargar.Size = new System.Drawing.Size(118, 39);
+            this.BtnCargar.TabIndex = 24;
+            this.BtnCargar.Text = "CARGAR";
+            this.BtnCargar.UseVisualStyleBackColor = false;
+            // 
+            // cmxCategoria
+            // 
+            this.cmxCategoria.FormattingEnabled = true;
+            this.cmxCategoria.Location = new System.Drawing.Point(171, 206);
+            this.cmxCategoria.Name = "cmxCategoria";
+            this.cmxCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cmxCategoria.TabIndex = 23;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(171, 167);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(121, 21);
+            this.cmbMarca.TabIndex = 22;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(171, 131);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(121, 20);
+            this.txtDescripcion.TabIndex = 21;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(171, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(45, 214);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(58, 13);
+            this.lblCategoria.TabIndex = 19;
+            this.lblCategoria.Text = "Categoria: ";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(45, 175);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 18;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(45, 138);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 17;
+            this.lblDescripcion.Text = "Descripcion:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(45, 96);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(50, 13);
+            this.lblNombre.TabIndex = 16;
+            this.lblNombre.Text = "Nombre: ";
+            // 
+            // txtCodArticulo
+            // 
+            this.txtCodArticulo.Location = new System.Drawing.Point(171, 54);
+            this.txtCodArticulo.Name = "txtCodArticulo";
+            this.txtCodArticulo.Size = new System.Drawing.Size(121, 20);
+            this.txtCodArticulo.TabIndex = 15;
+            // 
+            // lblCodigoArticulo
+            // 
+            this.lblCodigoArticulo.AutoSize = true;
+            this.lblCodigoArticulo.Location = new System.Drawing.Point(45, 57);
+            this.lblCodigoArticulo.Name = "lblCodigoArticulo";
+            this.lblCodigoArticulo.Size = new System.Drawing.Size(99, 13);
+            this.lblCodigoArticulo.TabIndex = 14;
+            this.lblCodigoArticulo.Text = "Codigo de Articulo: ";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.Beige;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBuscar.Location = new System.Drawing.Point(298, 48);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(70, 28);
+            this.btnBuscar.TabIndex = 28;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // ModificarArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(630, 394);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.TxtPrecio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnCargar);
+            this.Controls.Add(this.cmxCategoria);
+            this.Controls.Add(this.cmbMarca);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtCodArticulo);
+            this.Controls.Add(this.lblCodigoArticulo);
+            this.Name = "ModificarArticulo";
+            this.Text = "ModificarArticulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtPrecio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnCargar;
+        private System.Windows.Forms.ComboBox cmxCategoria;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtCodArticulo;
+        private System.Windows.Forms.Label lblCodigoArticulo;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
---
+++ tp/TpWinforms-Figueroa-Licla-Saavedra/TpWinforms-Figueroa-Licla-Saavedra/ModificarArticulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clases;
+using Conexiones;
+
+namespace TpWinforms_Figueroa_Licla_Saavedra
+{
+    public partial class ModificarArticulo : Form
+    {
+        public ModificarArticulo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Articulo articulo = new Articulo();
+            articulo.Codigo = txtCodArticulo.Text;
+            articulo = new ArticuloNegocio().BuscarArticuloid(articulo);
+
+            if(articulo.ID != 0)
+            {
+                txtCodArticulo.Text = articulo.Codigo;
+                txtDescripcion.Text = articulo.Descripcion;
+                txtNombre.Text = articulo.Nombre;
+                TxtPrecio.Text = ""+ (articulo.Precio);
+                
+            }
+            
+        }
+    }
+}
